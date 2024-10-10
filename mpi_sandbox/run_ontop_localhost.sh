9aedf0698f6ed583b5b846857478b52fb0554ec1
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2024-10-10 11:05:10 +0200

    Update mappings.
    WIP: MapAnnotations and KV pairs.

--- mpi_sandbox/run_ontop_localhost.sh
+++ mpi_sandbox/run_ontop_localhost.sh
@@ -5,4 +5,4 @@ MAPPING=omemap.obda
 ONTOLOGY=omemap.ttl
 PROPERTIES=mpi.properties
 
-$ONTOPBIN endpoint -m $MAPPING -t $ONTOLOGY -p $PROPERTIES 2>&1 > /dev/null
+$ONTOPBIN endpoint -m $MAPPING -t $ONTOLOGY -p $PROPERTIES
