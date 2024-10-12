bbc48baf11f67f2e1bfb2f1dc1a24abe2a27ca57
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2024-10-12 20:55:26 +0200

    Add script to CI
    - launch omero
    - insert data
    - launch ontop

---
+++ ci_utils/omero-ontop.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+set -e
+
+# Script to run as a single CI step in gh actions
+# Launch omero
+NOCLEAN=true .omero/docker dev start_up
+
+# Check omero-web is reachable
+curl http://localhost:14080
+
+# Inject data
+ci_utils/insert_data.sh
+
+# Spin up ontop
+ontop-cli/ontop endpoint -t mpieb/omemap.ttl -m mpieb/omemap.obda -p mpieb/omero-test-infra.properties 
+
+# Check sparql endpoint is reachable
+curl http://localhost:8080/sparql
