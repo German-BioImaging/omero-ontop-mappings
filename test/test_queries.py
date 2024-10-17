ba302a3795c1eec19a0e7d384f152e9af1c82e29
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2024-10-17 17:35:18 +0200

    Update mappings
    - debugged to get correct count of images, datasets, and projects.

--- test/test_queries.py
+++ test/test_queries.py
@@ -28,6 +28,28 @@ class QueriesTest(unittest.TestCase):
             elif os.path.isdir(item):
                 shutil.rmtree(item)
                 
+    def test_project(self):
+        """ Test number of projects in the VKG. """
+        
+        graph = self._graph
+
+        query_string = f"""
+        prefix ome_core: <http://www.openmicroscopy.org/rdf/2016-06/ome_core/>
+
+        SELECT distinct ?s WHERE {{
+          SERVICE <{ENDPOINT}> {{
+            ?ds a ome_core:Project .
+          }}
+        }}
+        limit 3
+        """
+
+        # Run the query.
+        response = graph.query(query_string)
+
+        # Test.
+        self.assertEqual(len(response), 1)
+ 
     def test_dataset(self):
         """ Test that there are 3 datasets in the graph db"""
         
@@ -49,7 +71,31 @@ class QueriesTest(unittest.TestCase):
 
         # Test.
         self.assertEqual(len(response), 3)
- 
+  
+    def test_image(self):
+        """ Test number of images in VKG. """
+        
+        graph = self._graph
+
+        query_string = f"""
+        prefix ome_core: <http://www.openmicroscopy.org/rdf/2016-06/ome_core/>
+
+        SELECT distinct ?s WHERE {{
+          SERVICE <{ENDPOINT}> {{
+            ?s a ome_core:Image .
+          }}
+        }}
+        limit 100
+        """
+
+        # Run the query.
+        response = graph.query(query_string)
+        for r in response:
+            print(r)
+
+        # Test.
+        self.assertEqual(len(response), 10)
+
     def test_project_dataset_image(self):
         """ Test a query for a project-dataset-image hierarchy. """
 
@@ -75,38 +121,6 @@ class QueriesTest(unittest.TestCase):
 
         # Should get 10 images.
         self.assertEqual(len(response), 10)
-
-    def test_mapannotation(self):
-        """ Test a query for map annotation, map, key, and value. """
-
-        graph = self._graph
-
-        query_string = f"""
-        prefix ome_core: <http://www.openmicroscopy.org/rdf/2016-06/ome_core/>
-
-        SELECT distinct ?key  WHERE {{
-          SERVICE <{ENDPOINT}> {{
-            ?ds a ome_core:Dataset;
-                ome_core:annotation ?annotation .
-            ?annotation ome_core:mapAnnotationValue ?map .
-            ?map ome_core:key ?keystr;
-                ome_core:value ?val ;
-                ome_core:nameSpace ?ns .
-            bind(iri(concat(str(?ns),str(?keystr))) as ?key)
-         }}
-        }}
-        limit 20
-        """
-
-        # Run the query.
-        response = graph.query(query_string)
-
-        keys = set([str(r.key) for r in response])
-
-        self.assertIn("http://purl.org/dc/terms/subject", keys)
-        self.assertIn("http://purl.org/dc/terms/contributor", keys)
-        self.assertIn("http://purl.org/dc/terms/provenance", keys)
-
 
     def test_image_key_value(self):
         """ Test querying for an image property via the mapannotation key."""
@@ -158,7 +172,7 @@ class QueriesTest(unittest.TestCase):
         for r in response:
             print(r.project, r.author, r.subject, r.provenance)
 
-        self.assertEqual(len(response), 4)
+        self.assertEqual(len(response), 1)
 
     def test_dataset_key_value(self):
         """ Test querying for an dataset property via the mapannotation key."""
