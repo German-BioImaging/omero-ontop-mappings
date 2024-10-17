93678e9c9d9dd7d9fa7e20978a068c94234c68a7
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2024-10-17 19:48:33 +0200

    Refactor util scripts re previous renames.

--- utils/omero-ontop.sh
+++ utils/omero-ontop.sh
@@ -10,10 +10,10 @@ NOCLEAN=true .omero/docker dev start_up
 
 # Inject data
 omero login -s localhost:14064 -u root -w omero
-ci_utils/insert_data.sh
+utils/insert_data.sh
 
 # Spin up ontop
-ontop-cli/ontop endpoint -t mpieb/omemap.ttl -m mpieb/omemap.obda -p mpieb/omero-test-infra.properties 
+ontop-cli/ontop endpoint -t test_infra_ontop/omemap.ttl -m test_infra_ontop/omemap.obda -p test_infra_ontop/omemap.properties 
 
 # Run a query (apache-jena/bin/ must be in $PATH)
-rsparql --service=http://localhost:8080/sparql --result=TSV --query=ci_utils/query01.rq > response01.tsv
+rsparql --service=http://localhost:8080/sparql --result=TSV --query=utils/query01.rq > response01.tsv
