9ac2b965f0fade167cf473bb5f07e5bc9c325b3f
Carsten <grotec@evolbio.mpg.de>
2025-06-02 14:43:32 +0200

    Add public user script

---
+++ utils/create_public_omero_user.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Create the public user in omero and configure omero to login as public by default
+omero login -C -u root -w omero
+omero group add --type read-only public
+omero user add -P public public Public User public
+omero config set omero.web.public.enabled True
+omero config set omero.web.public.user 'public'
+omero config set omero.web.public.password 'public'
+omero config set omero.web.public.server_id 1
